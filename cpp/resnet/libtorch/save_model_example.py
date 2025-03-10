cbdb533639ff00b6e9c9171d24139396a45a7386
vincent <891922758@qq.com>
2025-03-10 00:59:29 -0700

    feat: add an example to save jit model

---
+++ cpp/resnet/libtorch/save_model_example.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision.models as models
+
+
+# 1. get a model
+model = models.resnet18(pretrained=True).to("cpu")
+
+# 2. use torch.jit.trace to convert the model
+example_input = torch.rand(1, 3, 224, 224)  # needs an exmaple input
+traced_model = torch.jit.trace(model, example_input)
+
+# 3. save as TorchScript models
+traced_model.save("model_jit.pth")  # ✅ can be loade by lib_torch
